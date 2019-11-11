6f342d24084385617876cd1cbafa9a63000c0317
Tony4711 <tony.schultze96@gmail.com>
2019-11-11 16:14:57 +0100

    //

--- src/MetricDate.java
+++ src/MetricDate.java
@@ -4,7 +4,6 @@ public class MetricDate extends JulianDate{
 	private int metYear;
     private int metMonth;
     private int metDay;
-    //private double julian;
     
     public MetricDate() {
     	super();
@@ -14,20 +13,19 @@ public class MetricDate extends JulianDate{
 	{
     	MetricDate md = new MetricDate();
     	JulianDate Julian = new JulianDate();
-		//Print the returned Julian Date from the method, needs a parameter of  an Array that represents the todays date, takes the date from the Calendar instance of java
-		System.out.println(Julian.julianDateAlgorithm(Julian.setDate(1997, 3, 19)));
-    	md.metricDateAlgorithm();
-    	md.metricToJulian();
+    	double ju = Julian.julianDateAlgorithm(Julian.setDate(1997, 3, 19));
+    	md.metricDateAlgorithm(ju);
+    	md.metricToJulian(ju);
 	}
     
-    public void metricDateAlgorithm(){
+    public void metricDateAlgorithm(double julian){
         metYear = (int) (julian/1000);
         metMonth = (int) ((julian%1000)/100);
         metDay = (int) ((julian%1000)%100);
         System.out.println("Metric Date: "+metDay+"."+metMonth+"."+metYear);
         }
     
-    public void metricToJulian() {
+    public void metricToJulian(double julian) {
     	julian = (metYear * 1000) + (metMonth * 100) + metDay;
     	System.out.println("Metric date " + metDay + "." + metMonth + "." + metYear + " is equal to the Julian date " + julian);
     	}
